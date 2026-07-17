6b4e82180a7e0791cea0655fe469d150747c8144
dmitry_mih <dmih5678@gmail.com>
2026-07-18 00:52:37 +0300

    flowers rendering simple added

---
+++ ApiaryClient/src/components/Flower.vue
@@ -0,0 +1,58 @@
+<template>
+
+    <div class="flower" :style="{ left: x + 'px', top: y + 'px' }">
+        <p>🌼
+        <p class="nectar">
+            {{ nectarAmount }}
+        </p>
+        </p>
+    </div>
+
+
+
+</template>
+
+
+<script setup>
+
+defineProps(
+    {
+        x: {
+            type: Number,
+            required: true,
+        },
+        y: {
+            type: Number,
+            required: true,
+        },
+        nectarAmount: {
+            type: Number,
+            required: true,
+        }
+    })
+
+
+
+
+</script>
+
+
+
+<style lang="css" scoped>
+.flower {
+    position: absolute;
+    font-size: 44px;
+    width: 44px;
+    height: 44px;
+    transform: translate(-50%, -50%);
+    transition: left 0.15s ease-out, top 0.15s ease-out;
+    user-select: none;
+    pointer-events: none;
+    line-height: 1;
+    text-align: center;
+}
+
+.nectar {
+    font-size: 20px;
+}
+</style>
--- ApiaryClient/src/components/Game.vue
+++ ApiaryClient/src/components/Game.vue
@@ -1,23 +1,31 @@
 <template>
     <div class="gameMap">
         <Bee v-for="bee in bees" :key="bee.beeId" :x="bee.positionX" :y="bee.positionY" :id="bee.beeId" />
+
+        <Flower v-for="flower in flowers" :key="flower.flowerId" :x="flower.positionX" :y="flower.positionY"
+            :nectarAmount="flower.amountOfNectar"></Flower>
+
     </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
 import Bee from './Bee.vue'
+import Flower from './Flower.vue'
 
 const bees = ref([])
+const flowers = ref([])
 let sseListener = null
 
 onMounted(() => {
+
+    renderFlowers();
+
     sseListener = new EventSource('https://localhost:7257/api/ApiaryStates')
 
     sseListener.onmessage = (event) => {
         const data = JSON.parse(event.data)
         addOrUpdateBee(data)
-
     }
 })
 
@@ -26,11 +34,9 @@ onMounted(() => {
 function addOrUpdateBee(item) {
     const index = bees.value.findIndex(b => b.beeId === item.beeId)
 
-    const minX = 0, maxX = 100
-    const minY = 0, maxY = 100
-
-    const screenX = ((item.position.x - minX) / (maxX - minX)) * 1200
-    const screenY = 800 - ((item.position.y - minY) / (maxY - minY)) * 900
+    let coords = GetCoordinates(item.position.x, item.position.y)
+    let screenX = coords[0]
+    let screenY = coords[1]
 
     if (index !== -1) {
         bees.value[index].positionX = screenX
@@ -42,6 +48,46 @@ function addOrUpdateBee(item) {
             positionY: screenY
         })
     }
+}
+
+async function renderFlowers() {
+    let res = await fetch("https://localhost:7257/api/Flowers");
+    console.log(res)
+    let flowersResult = await res.json();
+
+    console.log(flowersResult)
+
+    for (let element of flowersResult) {
+        let coords = GetCoordinates(element.position.x, element.position.y)
+
+        let screenX = coords[0]
+        let screenY = coords[1]
+        console.log(coords)
+
+        flowers.value.push({
+            flowerId: element.flowerId,
+            positionX: screenX,
+            positionY: screenY,
+            amountOfNectar: element.amountOfNectar
+        })
+
+        console.log(flowers);
+    }
+}
+
+function mapEventToHandler(event) {
+
+}
+
+
+function GetCoordinates(positionX, positionY) {
+    const minX = 0, maxX = 100
+    const minY = 0, maxY = 100
+
+    const screenX = ((positionX - minX) / (maxX - minX)) * 1200
+    const screenY = 800 - ((positionY - minY) / (maxY - minY)) * 900
+
+    return [screenX, screenY]
 }
 </script>
 
